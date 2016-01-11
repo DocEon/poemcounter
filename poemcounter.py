5f5cf26e108fb8bce2f1e04ad90aa315fe5baeb4
Ken Alba <kenalba@bu.edu>
2016-01-11 14:47:28 -0500

    Refactoring
    Got rid of some lines I didn’t need, made the while condition neater,
    added some newlines to make output neater.

--- poemcounter.py
+++ poemcounter.py
@@ -5,13 +5,13 @@ import sys
 # give an option to not be case-sensitive with first-line matches.
 
 def main():
-	poemToRead = raw_input("What poem do you want to search through? Answers should be in the form poemname.txt\n")
-	keepGoing = True
-	while keepGoing:
+	poemToRead = raw_input("\nWhat poem do you want to search through? \nAnswers should be in the form poemname.txt\n\n")
+	nextCommand = ""
+	while nextCommand != "exit":
 		f = open(poemToRead)
 		totalLines = 0
 		numberOfStarters = 0
-		startingWordToLookFor = raw_input("What word are we looking for?\n")
+		startingWordToLookFor = raw_input("\nWhat word are we looking for?\n")
 		for line in f:
 			lineArray = []
 			lineArray = line.split()
@@ -19,16 +19,15 @@ def main():
 				totalLines += 1
 				if lineArray[0] == startingWordToLookFor:
 					numberOfStarters += 1
-		print numberOfStarters, "lines begin with", startingWordToLookFor
-		print "There are", totalLines, "lines with text on them."
+		print "\n", numberOfStarters, "lines begin with \"", startingWordToLookFor, "\"\n"
+		print "There are", totalLines, "lines with text on them.\n"
 		nextCommand = raw_input("Input your next command - exit to quit, again to search the same poem, new to search a new poem.\n")
 		if nextCommand == "new":
 			poemToRead = raw_input("What's the new poem you want to search through?")
-		elif nextCommand == "exit":
-			keepGoing = False
 
-print "Welcome to PoemCounter! This program takes two arguments, a starting word to look for and a file."
+print "\nWelcome to PoemCounter! This program takes two arguments, a starting word to look for and a file.\n"
 print "The file should be saved in the same folder as this snippet of code."
 print "This code is case-sensitive!"
-print "Note that total line counts ONLY remove blank lines - if there are stanza markers, you'll need to count those yourself."
+print "Note that total line counts ONLY remove blank lines."
+
 main()
